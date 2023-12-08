fe51143a6d8892d95ea44bdd92a0222531ce99bf
samfallahi <mfallahi@uncc.edu>
2023-12-08 15:22:39 -0500

    feat: returns embedding layer info

---
+++ data_handler/embedding_dim.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+
+def cal_dim(df, categorical_columns, label):
+    X = df.drop(label, axis=1)
+    numerical_columns = [col for col in df.columns if col not in categorical_columns]
+    categorical_columns.remove(label)
+    X_num = X[numerical_columns]
+    # Calculate the number of unique values per categorical column
+    cat_dims = [int(X[col].nunique()) for col in categorical_columns]
+    # Define the embedding size per categorical column (rule of thumb: min(50, (size+1)//2))
+    emb_dims = [(size, min(50, (size + 1) // 2)) for size in cat_dims]
+    num_feature_no = X_num.shape[1]
+    return num_feature_no, emb_dims
