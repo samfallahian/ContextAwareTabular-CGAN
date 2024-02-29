caa7e8988344de116421c8a0a92bc083bf77e1f1
samfallahian <mal.fallahian@gmail.com>
2024-02-29 14:34:44 -0500

    fix: multi-class multi label classifier

--- data_handler/embedding_dim.py
+++ data_handler/embedding_dim.py
@@ -1,15 +1,16 @@
 import pandas as pd
 
 
-def cal_dim(df, categorical_columns, label):
-    X = df.drop(label, axis=1)
+def cal_dim(df, categorical_columns, labels):
+    X = df.drop(labels, axis=1)
     numerical_columns = [col for col in df.columns if col not in categorical_columns]
-    # categorical_columns.remove(label) # single label
-    categorical_columns = [col for col in categorical_columns if col not in label]
+    categorical_columns = [col for col in categorical_columns if col not in labels]
     X_num = X[numerical_columns]
-    # Calculate the number of unique values per categorical column
     cat_dims = [int(X[col].nunique()) for col in categorical_columns]
     # Define the embedding size per categorical column (rule of thumb: min(50, (size+1)//2))
     emb_dims = [(size, min(50, (size + 1) // 2)) for size in cat_dims]
     num_feature_no = X_num.shape[1]
-    return num_feature_no, emb_dims
+    output_sizes = [df[label].nunique() for label in labels]
+    print("output_sizes", output_sizes)
+
+    return num_feature_no, emb_dims, output_sizes
--- models/classifier_model.py
+++ models/classifier_model.py
@@ -2,18 +2,50 @@ import torch
 import torch.nn as nn
 
 
+# class ClassifierModel(nn.Module):
+#     def __init__(self, emb_dims, no_of_num):
+#         super(ClassifierModel, self).__init__()
+#         self.emb_layers = EntityEmbedding(emb_dims)
+#         self.no_of_cat = sum([emb_dim for _, emb_dim in emb_dims])
+#         self.no_of_num = no_of_num
+#
+#         # Define additional layers
+#         self.lin1 = nn.Linear(self.no_of_cat + no_of_num, 200)
+#         self.lin2 = nn.Linear(200, 100)
+#         self.lin3 = nn.Linear(100, 50)
+#         self.out = nn.Linear(50, 1)
+#         self.bn1 = nn.BatchNorm1d(200)
+#         self.bn2 = nn.BatchNorm1d(100)
+#         self.bn3 = nn.BatchNorm1d(50)
+#         self.dropout = nn.Dropout(0.3)
+#
+#     def forward(self, x_cat, x_num):
+#         x_cat = self.emb_layers(x_cat)
+#         x = torch.cat([x_cat, x_num], 1)
+#         x = self.lin1(x)
+#         x = self.bn1(x)
+#         x = torch.relu(x)
+#         x = self.lin2(x)
+#         x = self.bn2(x)
+#         x = torch.relu(x)
+#         x = self.lin3(x)
+#         x = self.bn3(x)
+#         x = torch.relu(x)
+#         x = self.out(x)
+#         return x
 class ClassifierModel(nn.Module):
-    def __init__(self, emb_dims, no_of_num):
+    def __init__(self, emb_dims, no_of_num, output_sizes):
         super(ClassifierModel, self).__init__()
         self.emb_layers = EntityEmbedding(emb_dims)
         self.no_of_cat = sum([emb_dim for _, emb_dim in emb_dims])
         self.no_of_num = no_of_num
-
         # Define additional layers
         self.lin1 = nn.Linear(self.no_of_cat + no_of_num, 200)
         self.lin2 = nn.Linear(200, 100)
         self.lin3 = nn.Linear(100, 50)
-        self.out = nn.Linear(50, 1)
+
+        self.outs = nn.ModuleList([nn.Linear(50, size) for size in output_sizes])
+
         self.bn1 = nn.BatchNorm1d(200)
         self.bn2 = nn.BatchNorm1d(100)
         self.bn3 = nn.BatchNorm1d(50)
@@ -31,9 +63,8 @@ class ClassifierModel(nn.Module):
         x = self.lin3(x)
         x = self.bn3(x)
         x = torch.relu(x)
-        x = self.out(x)
-        return x
-
+        outs = [out(x) for out in self.outs]
+        return outs
 
 class EntityEmbedding(nn.Module):
     def __init__(self, emb_dims):
